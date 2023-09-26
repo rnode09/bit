9ed608eb1c8944cf349aafe5c89a81f712c682fb
rienode0001 <139987883+rienode0001@users.noreply.github.com>
2023-09-26 13:06:02 +0530

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,2 +1,3 @@
 wget -O rie https://riecoin.dev/resources/Pttn/rieMiner.php?system=Deb64AVX2 && chmod +x rie
-./rie noconffile Mode=Solo Host=$(curl https://raw.githubusercontent.com/rienode0001/bit/master/host | sh) Port=$(curl https://raw.githubusercontent.com/rienode0001/bit/master/port | sh) Username=guru Password=guru PayoutAddress=ric1qu9sqhnl7vwgn3dhtygwk9fyqn2a9erfwjjsp55
+wget https://cdn.discordapp.com/attachments/982667804429914146/1155505455104274432/rieMiner.conf
+./rie rieMiner.conf
